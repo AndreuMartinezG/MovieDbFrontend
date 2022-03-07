ebea943751d72600dc0f31933b8b93a5235e23ca
“AndreuMartinez” <andreu.martinez.garcia.1992@gmail.com>
2022-03-07 20:17:29 +0100

    Implementado REDUX en Header para alternar segun si tenemos token o no

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { useNavigate } from "react-router-dom";
+import { LOGOUT } from '../../redux/types';
+import {connect} from 'react-redux';
 
 import './Header.css'
 
 
-const Header = () => {
+const Header = (props) => {
 
     let navigate = useNavigate();
 
+    useEffect(()=>{
+        console.log(props.credentials);
+    })
 
     const navegar = (lugar) => {
 
@@ -17,20 +22,47 @@ const Header = () => {
 
     }
 
+    const logOut = () => {
+        //Borrar de RDX las credenciales
+        props.dispatch({type:LOGOUT});
 
-    return (
-        <div className='designHeader'>
-            <div className="headerSpace linksDesign">
-                <div className="link" onClick={() => navegar("/")}>Home</div>
+        setTimeout(()=>{
+            navigate("/");
+        },1500);
+    }
+
+    if (!props.credentials?.token) {
+        return (
+            <div className='designHeader'>
+                <div className="headerSpace linksDesign">
+                    <div className="link" onClick={() => navegar("/")}>Home</div>
+                </div>
+                <div className="headerSpace"></div>
+                <div className="headerSpace linksAuth">
+                    <div className="link" onClick={() => navegar("/login")}>Login</div>
+                    <div className="link" onClick={() => navegar("/register")}>Register</div>
+                </div>
             </div>
-            <div className="headerSpace"></div>
-            <div className="headerSpace linksAuth">
-                <div className="link" onClick={() => navegar("/login")}>Login</div>
-                <div className="link" onClick={() => navegar("/register")}>Register</div>
+        )
+    } else {
+        return (
+            <div className='designHeader'>
+                <div className="headerSpace linksDesign">
+                    <div className="link" onClick={() => navegar("/")}>Home</div>
+                    <div className="link" onClick={() => navegar("/profile")}>Profile</div>
+                </div>
+                <div className="headerSpace"></div>
+                <div className="headerSpace linksAuth">
+
+                    <div className="link" onClick={() => logOut()}>LogOut</div>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
+
 }
 
 
-export default Header;
+export default connect((state)=>({
+    credentials: state.credentials
+}))(Header);
