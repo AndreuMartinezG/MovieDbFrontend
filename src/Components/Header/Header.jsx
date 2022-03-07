b4e43930667425ead30ef663e7d1dc33db2586dd
“AndreuMartinez” <andreu.martinez.garcia.1992@gmail.com>
2022-03-07 19:53:23 +0100

    Descargadas dependencias de redux

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,13 +1,33 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 import './Header.css'
 
 
 const Header = () => {
 
+    let navigate = useNavigate();
+
+
+    const navegar = (lugar) => {
+
+        setTimeout(() => {
+            navigate(lugar);
+        }, 200);
+
+    }
+
+
     return (
-        <div className="designHeader">
-            Soy Header
+        <div className='designHeader'>
+            <div className="headerSpace linksDesign">
+                <div className="link" onClick={() => navegar("/")}>Home</div>
+            </div>
+            <div className="headerSpace"></div>
+            <div className="headerSpace linksAuth">
+                <div className="link" onClick={() => navegar("/login")}>Login</div>
+                <div className="link" onClick={() => navegar("/register")}>Register</div>
+            </div>
         </div>
     )
 }
