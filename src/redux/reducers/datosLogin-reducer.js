24244fc8e1afbf15a430bb6251251a812a085220
“AndreuMartinez” <andreu.martinez.garcia.1992@gmail.com>
2022-03-07 20:00:31 +0100

    Creada estructura Basica de archivos y directorios para REDUX

---
+++ src/redux/reducers/datosLogin-reducer.js
@@ -0,0 +1,23 @@
+import { LOGIN, LOGOUT } from '../types';
+
+const initialState = {
+    token: '',
+    usuario: {}
+};
+
+const datosLoginReducer = (state = initialState, action) => {
+    switch (action.type) {
+        //GUARDO EN EL ESTADO LOS DATOS DEL USUARIO LOGUEADO
+        case LOGIN:
+            return action.payload;
+
+        //BORRAMOS DATOS GUARDADOS DE USUARIO LOGUEADO Y DEJAMOS VALORES VACIOS
+        case LOGOUT:
+            return initialState;
+
+        default:
+            return state
+    }
+}
+
+export default datosLoginReducer;
