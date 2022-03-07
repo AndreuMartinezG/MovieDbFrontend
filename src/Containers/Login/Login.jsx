61a4ea63eab6104414466108ca37149030062af0
“AndreuMartinez” <andreu.martinez.garcia.1992@gmail.com>
2022-03-07 20:12:16 +0100

    Añadida estructura basica al container LOGIN

--- src/Containers/Login/Login.jsx
+++ src/Containers/Login/Login.jsx
@@ -1,16 +1,81 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from 'axios'
+
+
+//REDUX
+
+import { connect } from 'react-redux'
+import { LOGIN } from '../../redux/types'
 
 import './Login.css'
 
-const Login = () => {
+const Login = (props) => {
+
+    let navigate = useNavigate();
+
+    //1-Hooks (equivalen al estado en los componentes de clase)
+    const [datosUsuario, setDatosUsuario] = useState({ email: "", password: "" });
+    const [msgError, setMsgError] = useState("");
+    const [msgError2, setMsgError2] = useState("");
 
 
+
+    //Funciones handlers
+    const rellenarDatos = (e) => {
+        //Funcion handler que setea los datos en el hook...[e.target.name] obtiene 
+        //el nombre de la propiedad a cambiar, e.target.value tiene el valor..ambos
+        //obtienen los datos del evento, que es el hecho de escribir en un input en concreto
+        setDatosUsuario({ ...datosUsuario, [e.target.name]: e.target.value })
+    };
+
+    const login = async () => {
+
+        try {
+
+            //Me invento las credenciales
+            let body = {
+                email: datosUsuario.email,
+                password: datosUsuario.password
+            }
+
+            let resultado = await axios.post("https://movie-db-geekshubs.herokuapp.com/usuarios/login", body);
+
+            //Cambiamos el valor del hook credenciales, por lo tanto se recargará el componente
+            if (resultado.data === "Usuario o contraseña inválido") {
+                setMsgError2("Usuario o contraseña inválido")
+            } else {
+
+                //Guardaríamos los datos en redux...
+
+                props.dispatch({ type: LOGIN, payload: resultado.data });
+
+
+                setTimeout(() => {
+                    navigate("/");
+                }, 1500);
+            }
+
+
+        } catch (error) {
+
+            console.log(error)
+
+        }
+    }
     return (
         <div className="designLogin">
-            Soy login
+            {<pre>{JSON.stringify(datosUsuario, null, 2)}</pre>}
+            <div className="designFormulario">
+                <input type="email" name="email" id="email" title="email" placeholder="Correo Electrónico" autoComplete="off" onChange={(e) => { rellenarDatos(e) }} />
+                <input type="password" name="password" id="password" title="password" placeholder="Contraseña" autoComplete="off" onChange={(e) => { rellenarDatos(e); }} />
+                {msgError}
+                {msgError2}
+            </div>
+            <div className="loginButton espacio" onClick={() => login()}>LOG ME!</div>
         </div>
+
     )
 }
 
-
-export default Login;
+export default connect()(Login);
