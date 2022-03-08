8736743501eddc092ce4d515c893ec449262b7b1
“AndreuMartinez” <andreu.martinez.garcia.1992@gmail.com>
2022-03-08 21:52:21 +0100

    Diseño basico de los elementos en SideBarProfile

--- src/Components/SideBarProfile/SideBarProfile.jsx
+++ src/Components/SideBarProfile/SideBarProfile.jsx
@@ -1,34 +1,45 @@
 import './SideBarProfile.css';
 import { useState } from 'react';
 import { Drawer, Burger } from '@mantine/core';
+import { useNavigate } from "react-router-dom";
 
 
 function SideBarProfile() {
-  const [opened, setOpened] = useState(false);
 
-  return (
-    <>
-      <Drawer className='color'
-        opened={opened}
-        onClose={() => setOpened(false)}
-        title=""
-        padding="xl"
-        size="50vw"
-        position='left'
-      >
-        <navbar className="flex">
-          <h1 className='titulo-lateral'>Tu Perfil:</h1>
-          <div className='bt-lateral'>item 1</div>
-          <div className='bt-lateral'>item 2</div>
-          <div className='bt-lateral'>item 3</div>
-          <div className='bt-lateral'>item 4</div>
+    let navigate = useNavigate();
 
-        </navbar>
-      </Drawer>
+    const [opened, setOpened] = useState(false);
 
-      <Burger opened={opened} onClick={() => setOpened(true)}>Open Drawer</Burger>
-    </>
-  );
+    const navegar = (lugar) => {
+
+        setTimeout(() => {
+            navigate(lugar);
+        }, 200);
+
+    }
+
+    return (
+        <>
+            <Drawer
+                opened={opened}
+                onClose={() => setOpened(false)}
+                title=""
+                padding="xl"
+                size="50vw"
+                position='left'
+            >
+
+                <h1 className='titulo-lateral' >Opciones:</h1>
+                <div className='bt-lateral' onClick={() => navegar("/")}>Tus Datos de Perfil</div>
+                <div className='bt-lateral'>item 2</div>
+                <div className='bt-lateral'>item 3</div>
+                <div className='bt-lateral'>item 4</div>
+
+            </Drawer>
+
+            <Burger opened={opened} onClick={() => setOpened(true)}>Open Drawer</Burger>
+        </>
+    );
 }
 
 export default SideBarProfile;
