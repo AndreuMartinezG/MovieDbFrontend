aa6df6232faf902b679fceb8e7f3ab6c75e22fd4
“AndreuMartinez” <andreu.martinez.garcia.1992@gmail.com>
2022-03-08 22:37:12 +0100

    Añadido funcion para traer datos del Usuario en /DatosPerfil

--- src/Containers/DatosPerfil/DatosPerfil.jsx
+++ src/Containers/DatosPerfil/DatosPerfil.jsx
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './DatosPerfil.css'
 import SideBarProfile from "../../Components/SideBarProfile/SideBarProfile";
+import { useNavigate } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 
-const DatosPerfil = () => {
+
+const DatosPerfil = (props) => {
+
+    let navigate = useNavigate()
+
+
+    useEffect(() => {
+        if (props.credentials.token === '') {
+            navigate("/");
+        }
+    })
 
     return (
         <div className="designDatosPerfil">
             <SideBarProfile />
-            Soy Los Datos de Tu perfil
+            <div className="designProfileHalf profileLeft">
+                <div className="profileField">Nombre: {props.credentials.usuario.nombre} </div>
+                <div className="profileField">Apellidos: {props.credentials.usuario.apellido}
+                </div>
+                <div className="profileField">Email: {props.credentials.usuario.email}</div>
+                <div className="profileField">Teléfono: {props.credentials.usuario.telefono}</div>
+                <div className="profileField">N. cuenta: {props.credentials.usuario.numCuenta}</div>
+            </div>
+
         </div>
     )
 }
 
-export default DatosPerfil;
+export default connect((state) => ({
+    credentials: state.credentials
+}))(DatosPerfil);
