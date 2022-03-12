c046edb1eff3c0c6db5f032988358c1348e81fa7
“AndreuMartinez” <andreu.martinez.garcia.1992@gmail.com>
2022-03-12 23:18:05 +0100

    feat: Implementado diseño a LogIn con validacion de errores

--- src/Containers/Login/Login.jsx
+++ src/Containers/Login/Login.jsx
@@ -2,7 +2,10 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from 'axios'
 
-
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+import { Form, Input, Button, Checkbox, Typography, } from 'antd';
+import {LockOutlined, UserOutlined} from '@ant-design/icons'
 //REDUX
 
 import { connect } from 'react-redux'
@@ -10,46 +13,65 @@ import { LOGIN } from '../../redux/types'
 
 import './Login.css'
 
+const { Title } = Typography;
+
 const Login = (props) => {
 
     let navigate = useNavigate();
 
     //1-Hooks (equivalen al estado en los componentes de clase)
     const [datosUsuario, setDatosUsuario] = useState({ email: "", password: "" });
-    const [msgError, /*setMsgError*/] = useState("");
-    const [msgError2, setMsgError2] = useState("");
 
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////
+
+    const rememberMeChecked = localStorage.getItem("rememberMe") ? true : false;
+
+    const [formErrorMessage, setFormErrorMessage] = useState('')
+    const [rememberMe, setRememberMe] = useState(rememberMeChecked)
+
+    const handleRememberMe = () => {
+        setRememberMe(!rememberMe)
+    };
+
+    const initialEmail = localStorage.getItem("rememberMe") ? localStorage.getItem("rememberMe") : '';
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
 
 
     //Funciones handlers
-    const rellenarDatos = (e) => {
-        setDatosUsuario({ ...datosUsuario, [e.target.name]: e.target.value })
-    };
+    // const rellenarDatos = (e) => {
+    //     setDatosUsuario({ ...datosUsuario, [e.target.name]: e.target.value })
+    // };
 
 
-    
+
     useEffect(() => {
         if (props.credentials?.token) {
             navigate("/");
         }
     })
 
-    const login = async () => {
+    const login = async (dataToSubmit) => {
 
         try {
 
             //Me invento las credenciales
-            
-            let body = {
-                email: datosUsuario.email,
-                password: datosUsuario.password
-            }
 
+            let body = dataToSubmit
+            
+
+            console.log("envio del body", body)
             let resultado = await axios.post("https://movie-db-geekshubs.herokuapp.com/usuarios/login", body);
+            console.log("resultado", resultado)
 
             //Cambiamos el valor del hook credenciales, por lo tanto se recargará el componente
             if (resultado.data === "Usuario o contraseña inválido") {
-                setMsgError2("Usuario o contraseña inválido")
+                alert("Usuario o contraseña inválido")
+                setTimeout(() => {
+                    window.location.reload()
+                },1000)
+                navigate("/login")
             } else {
 
                 //Guardaríamos los datos en redux...
@@ -72,14 +94,110 @@ const Login = (props) => {
 
 
     return (
-        <div className="designLogin">
-            {<pre>{JSON.stringify(datosUsuario, null, 2)}</pre>}
-            <div className="designFormulario">
-                <input type="email" name="email" id="email" title="email" placeholder="Correo Electrónico" autoComplete="off" onChange={(e) => { rellenarDatos(e) }} />
-                <input type="password" name="password" id="password" title="password" placeholder="Contraseña" autoComplete="off" onChange={(e) => { rellenarDatos(e); }} />
-            </div>
-            <div className="loginButton espacio" onClick={() => login()}>LOG ME!</div>
-        </div>
+        <Formik
+            initialValues={{
+                email: initialEmail,
+                password: '',
+            }}
+            validationSchema={Yup.object().shape({
+                email: Yup.string()
+                    .email('Email is invalid')
+                    .required('Email is required'),
+                password: Yup.string()
+                    .min(4, 'Password must be at least 6 characters')
+                    .required('Password is required'),
+            })}
+            onSubmit={(values, { setSubmitting }) => {
+                console.log(values)
+                setTimeout(() => {
+                    let dataToSubmit = {
+                        email: values.email,
+                        password: values.password
+                    };
+
+                    login(dataToSubmit)
+                }, 500);
+
+
+            }}
+        >
+            {props => {
+                const {
+                    values,
+                    touched,
+                    errors,
+                    dirty,
+                    isSubmitting,
+                    handleChange,
+                    handleBlur,
+                    handleSubmit,
+                    handleReset,
+                } = props;
+                return (
+                    <div className="app">
+
+                        <Title level={1}>Log In</Title>
+                        <hr />
+                        <form onSubmit={handleSubmit} style={{ width: '350px' }}>
+
+                            <Form.Item required>
+                                <UserOutlined />
+                                <Input
+                                    id="email"
+                                    placeholder="Enter your email"
+                                    type="email"
+                                    value={values.email}
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    className={
+                                        errors.email && touched.email ? 'text-input error' : 'text-input'
+                                    }
+                                />
+                                {errors.email && touched.email && (
+                                    <div className="input-feedback">{errors.email}</div>
+                                )}
+                            </Form.Item>
+
+                            <Form.Item required>
+                                <LockOutlined />
+                                <Input
+                                    id="password"
+                                    
+                                    placeholder="Enter your password"
+                                    type="password"
+                                    value={values.password}
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    className={
+                                        errors.password && touched.password ? 'text-input error' : 'text-input'
+                                    }
+                                />
+                                {errors.password && touched.password && (
+                                    <div className="input-feedback">{errors.password}</div>
+                                )}
+                            </Form.Item>
+
+                            {formErrorMessage && (
+                                <label ><p style={{ color: '#ff0000bf', fontSize: '0.7rem', border: '1px solid', padding: '1rem', borderRadius: '10px' }}>{formErrorMessage}</p></label>
+                            )}
+
+                            <Form.Item>
+                                <Checkbox className="checkIzq" id="rememberMe" onChange={handleRememberMe} checked={rememberMe} >Remember me</Checkbox>
+                                <a className="login-form-forgot" href="/reset_user" style={{ float: 'right' }}>
+                                    forgot password
+                                </a>
+                                <div>
+                                    <Button type="primary" htmlType="submit" className="login-form-button" style={{ minWidth: '100%' }} disabled={isSubmitting} onSubmit={handleSubmit}>
+                                        Log in
+                                    </Button>
+                                </div>
+                                Or <a href="/register">register now!</a>
+                            </Form.Item>
+                        </form>
+                    </div>
+                );
+            }}
+        </Formik>
 
     )
 }
@@ -88,3 +206,14 @@ const Login = (props) => {
 export default connect((state) => ({
     credentials: state.credentials
 }))(Login);
+
+
+
+/*{ <div className="designLogin">
+    {<pre>{JSON.stringify(datosUsuario, null, 2)}</pre>}
+    <div className="designFormulario">
+        <input type="email" name="email" id="email" title="email" placeholder="Correo Electrónico" autoComplete="off" onChange={(e) => { rellenarDatos(e) }} />
+        <input type="password" name="password" id="password" title="password" placeholder="Contraseña" autoComplete="off" onChange={(e) => { rellenarDatos(e); }} />
+    </div>
+    <div className="loginButton espacio" onClick={() => login()}>LOG ME!</div>
+</div>} */
