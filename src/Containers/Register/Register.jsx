5e168ccd620db13b7eeafcab22f1a55743831b9f
“AndreuMartinez” <andreu.martinez.garcia.1992@gmail.com>
2022-03-13 11:59:04 +0100

    feat: Primera aproximacion de validacion de Registro con YUP

--- src/Containers/Register/Register.jsx
+++ src/Containers/Register/Register.jsx
@@ -2,8 +2,42 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import './Register.css';
+import moment from "moment";
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+import {
+    Form,
+    Input,
+    Button,
+  } from 'antd';
 
 import { connect } from 'react-redux';
+
+
+const formItemLayout = {
+    labelCol: {
+      xs: { span: 24 },
+      sm: { span: 8 },
+    },
+    wrapperCol: {
+      xs: { span: 24 },
+      sm: { span: 16 },
+    },
+  };
+  const tailFormItemLayout = {
+    wrapperCol: {
+      xs: {
+        span: 24,
+        offset: 0,
+      },
+      sm: {
+        span: 16,
+        offset: 8,
+      },
+    },
+  };
+
+
 
 const Register = (props) => {
 
@@ -21,21 +55,189 @@ const Register = (props) => {
         }
     })
 
-    // useEffect(()=>{
-    //     //useEffect observable que sólo se ejecutará cuando
-    //     //datosUsuario mute
-    // },
-    // [datosUsuario])
+    const phoneRegExp = '/^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/ '
+    //const dispatch = useDispatch();
+    return (
+  
+      <Formik
+        initialValues={{
+            nombre: '',
+            apellido: '',
+            email: '',
+            edad: '',
+            telefono: '',
+            password: '',
+            confirmPassword: ''
+        }}
+
+        
+
+        validationSchema={Yup.object().shape({
+            nombre: Yup.string()
+                .required('Se requiere Nombre'),
+            apellido: Yup.string()
+                .required('Se requiere Apellido'),
+            email: Yup.string()
+                .email('Email invalido')
+                .required('Se requiere Email'),
+            edad: Yup.number()
+                .typeError('Debes especificar un numero')
+                .min(18, 'Edad minima 18')
+                .max(99, 'Edad maxima 99')
+                .required('Se requiere Edad'),
+            telefono: Yup.string()
+                .matches(phoneRegExp, 'Telefono no valido')
+                .required('Se requiere tu Telefono'),
+            password: Yup.string()
+                .min(4, 'La contraseña requiere 4 caracteres')
+                .required('Se requiere Contraseña'),
+            confirmPassword: Yup.string()
+                .oneOf([Yup.ref('password'), null], 'Las Contraseñas deben coincidir')
+                .required('Se requiere validacion de Contraseña')
+        })}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+  
+            let dataToSubmit = {
+                
+              nombre: values.nombre,
+              apellido: values.apellido,
+              email: values.email,
+              edad: values.edad,
+              telefono: values.telefono,
+              password: values.password,
+              //image: `http://gravatar.com/avatar/${moment().unix()}?d=identicon`
+            };
+  
+            // dispatch(registerUser(dataToSubmit)).then(response => {
+            //   if (response.payload.success) {
+            //     props.history.push("/login");
+            //   } else {
+            //     alert(response.payload.err.errmsg)
+            //   }
+            // })
+  
+            setSubmitting(false);
+          }, 500);
+        }}
+      >
+        {props => {
+          const {
+            values,
+            touched,
+            errors,
+            dirty,
+            isSubmitting,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            handleReset,
+          } = props;
+          return (
+            <div className="app">
+              <h1>Registro</h1>
+              <Form style={{ minWidth: '375px' }} {...formItemLayout} onSubmit={handleSubmit} >
+  
+                <Form.Item required label="Nombre">
+                  <Input
+                    id="nombre"
+                    placeholder="Introduce tu nombre"
+                    type="text"
+                    value={values.nombre}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    className={
+                      errors.nombre && touched.nombre ? 'text-input error' : 'text-input'
+                    }
+                  />
+                  {errors.nombre && touched.nombre && (
+                    <div className="input-feedback">{errors.nombre}</div>
+                  )}
+                </Form.Item>
+  
+                <Form.Item required label="Apellido" hasFeedback validateStatus={errors.apellido && touched.apellido ? "error" : 'success'}>
+                  <Input
+                    id="apellido"
+                    placeholder="Enter your Apellido"
+                    type="text"
+                    value={values.apellido}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    className={
+                      errors.apellido && touched.apellido ? 'text-input error' : 'text-input'
+                    }
+                  />
+                  {errors.apellido && touched.apellido && (
+                    <div className="input-feedback">{errors.apellido}</div>
+                  )}
+                </Form.Item>
+  
+                <Form.Item required label="Email" hasFeedback validateStatus={errors.email && touched.email ? "error" : 'success'}>
+                  <Input
+                    id="email"
+                    placeholder="Enter your Email"
+                    type="email"
+                    value={values.email}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    className={
+                      errors.email && touched.email ? 'text-input error' : 'text-input'
+                    }
+                  />
+                  {errors.email && touched.email && (
+                    <div className="input-feedback">{errors.email}</div>
+                  )}
+                </Form.Item>
+  
+                <Form.Item required label="Password" hasFeedback validateStatus={errors.password && touched.password ? "error" : 'success'}>
+                  <Input
+                    id="password"
+                    placeholder="Enter your password"
+                    type="password"
+                    value={values.password}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    className={
+                      errors.password && touched.password ? 'text-input error' : 'text-input'
+                    }
+                  />
+                  {errors.password && touched.password && (
+                    <div className="input-feedback">{errors.password}</div>
+                  )}
+                </Form.Item>
+  
+                <Form.Item required label="Confirm" hasFeedback>
+                  <Input
+                    id="confirmPassword"
+                    placeholder="Enter your confirmPassword"
+                    type="password"
+                    value={values.confirmPassword}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    className={
+                      errors.confirmPassword && touched.confirmPassword ? 'text-input error' : 'text-input'
+                    }
+                  />
+                  {errors.confirmPassword && touched.confirmPassword && (
+                    <div className="input-feedback">{errors.confirmPassword}</div>
+                  )}
+                </Form.Item>
+  
+                <Form.Item {...tailFormItemLayout}>
+                  <Button onClick={handleSubmit} type="primary" disabled={isSubmitting}>
+                    Submit
+                  </Button>
+                </Form.Item>
+              </Form>
+            </div>
+          );
+        }}
+      </Formik>
+    );
 
 
     
 
-    return (
-        <div className='designRegister'>
-
-
-        </div>
-    )
 
 }
 
