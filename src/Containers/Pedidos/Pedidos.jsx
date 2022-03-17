ba7570d331029222a776ca9d4bd82806562a0e7d
“AndreuMartinez” <andreu.martinez.garcia.1992@gmail.com>
2022-03-17 16:33:02 +0100

    feat: render Basico de los pedidos del usuario

--- src/Containers/Pedidos/Pedidos.jsx
+++ src/Containers/Pedidos/Pedidos.jsx
@@ -1,4 +1,4 @@
-import React , { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import SideBarProfile from "../../Components/SideBarProfile/SideBarProfile";
 import { connect } from 'react-redux';
@@ -9,7 +9,8 @@ import './Pedidos.css'
 
 const Pedidos = (props) => {
 
-    
+    const [Pedidos, setPedidos] = useState([])
+
     let navigate = useNavigate()
 
     useEffect(() => {
@@ -22,8 +23,8 @@ const Pedidos = (props) => {
         }
 
     })
-    
-    
+
+
     const traerPedidos = async () => {
 
         let config = {
@@ -36,21 +37,44 @@ const Pedidos = (props) => {
             console.log(body, "ESTO ES EL BODY")
             let resultado = await axios.get(`https://movie-db-geekshubs.herokuapp.com/pedidos/${props.credentials.usuario.id}`, config);
 
-            console.log(resultado)
+            console.log(resultado.data)
+            setPedidos(resultado.data)
 
-        }catch (error) {
+        } catch (error) {
             console.log(error)
         }
     }
 
+    const renderCarrito = Pedidos.map((value, index) => {
+
+        const Button = ({ type }) => {
+            return <button className={"widgetLgButton " + type}>{type}</button>;
+        };
+
+        return (
+            <tr key={index} className="widgetLgTr">
+                <td className="widgetLgUser">
+                    <img
+                        src="https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                        alt=""
+                        className="widgetLgImg"
+                    />
+                    <span className="widgetLgName">{value.peliculaId}</span>
+                </td>
+                <td className="widgetLgDate">{value.fechaEntrega}</td>
+                <td className="widgetLgAmount">{value.precio}</td>
+                <td className="widgetLgStatus">
+                    <Button type="Approved" />
+                </td>
+            </tr>
+        )
+
+    })
 
 
 
 
 
-    const Button = ({ type }) => {
-        return <button className={"widgetLgButton " + type}>{type}</button>;
-    };
 
     return (
         <div className="designPedidos">
@@ -61,118 +85,14 @@ const Pedidos = (props) => {
                     <table className="widgetLgTable">
                         <thead>
                             <tr className="widgetLgTr">
-                                <th className="widgetLgTh">Customer</th>
+                                <th className="widgetLgTh">Film</th>
                                 <th className="widgetLgTh">Date</th>
-                                <th className="widgetLgTh">Amount</th>
+                                <th className="widgetLgTh">Price</th>
                                 <th className="widgetLgTh">Status</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr className="widgetLgTr">
-                                <td className="widgetLgUser">
-                                    <img
-                                        src="https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-                                        alt=""
-                                        className="widgetLgImg"
-                                    />
-                                    <span className="widgetLgName">Susan Carol</span>
-                                </td>
-                                <td className="widgetLgDate">2 Jun 2021</td>
-                                <td className="widgetLgAmount">$122.00</td>
-                                <td className="widgetLgStatus">
-                                    <Button type="Approved" />
-                                </td>
-                            </tr>
-                            <tr className="widgetLgTr">
-                                <td className="widgetLgUser">
-                                    <img
-                                        src="https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-                                        alt=""
-                                        className="widgetLgImg"
-                                    />
-                                    <span className="widgetLgName">Susan Carol</span>
-                                </td>
-                                <td className="widgetLgDate">2 Jun 2021</td>
-                                <td className="widgetLgAmount">$122.00</td>
-                                <td className="widgetLgStatus">
-                                    <Button type="Declined" />
-                                </td>
-                            </tr>
-                            <tr className="widgetLgTr">
-                                <td className="widgetLgUser">
-                                    <img
-                                        src="https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-                                        alt=""
-                                        className="widgetLgImg"
-                                    />
-                                    <span className="widgetLgName">Susan Carol</span>
-                                </td>
-                                <td className="widgetLgDate">2 Jun 2021</td>
-                                <td className="widgetLgAmount">$122.00</td>
-                                <td className="widgetLgStatus">
-                                    <Button type="Pending" />
-                                </td>
-                            </tr>
-                            <tr className="widgetLgTr">
-                                <td className="widgetLgUser">
-                                    <img
-                                        src="https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-                                        alt=""
-                                        className="widgetLgImg"
-                                    />
-                                    <span className="widgetLgName">Susan Carol</span>
-                                </td>
-                                <td className="widgetLgDate">2 Jun 2021</td>
-                                <td className="widgetLgAmount">$122.00</td>
-                                <td className="widgetLgStatus">
-                                    <Button type="Approved" />
-                                </td>
-                            </tr>
-                            <tr className="widgetLgTr">
-                                <td className="widgetLgUser">
-                                    <img
-                                        src="https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-                                        alt=""
-                                        className="widgetLgImg"
-                                    />
-                                    <span className="widgetLgName">Susan Carol</span>
-                                </td>
-                                <td className="widgetLgDate">2 Jun 2021</td>
-                                <td className="widgetLgAmount">$122.00</td>
-                                <td className="widgetLgStatus">
-                                    <Button type="Declined" />
-                                </td>
-                            </tr>
-                            <tr className="widgetLgTr">
-                                <td className="widgetLgUser">
-                                    <img
-                                        src="https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-                                        alt=""
-                                        className="widgetLgImg"
-                                    />
-                                    <span className="widgetLgName">Susan Carol</span>
-                                </td>
-                                <td className="widgetLgDate">2 Jun 2021</td>
-                                <td className="widgetLgAmount">$122.00</td>
-                                <td className="widgetLgStatus">
-                                    <Button type="Pending" />
-                                </td>
-                            </tr>
-                            <tr className="widgetLgTr">
-                                <td className="widgetLgUser">
-                                    <img
-                                        src="https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-                                        alt=""
-                                        className="widgetLgImg"
-                                    />
-                                    <span className="widgetLgName">Susan Carol</span>
-                                </td>
-                                <td className="widgetLgDate">2 Jun 2021</td>
-                                <td className="widgetLgAmount">$122.00</td>
-                                <td className="widgetLgStatus">
-                                    <Button type="Approved" />
-                                </td>
-                            </tr>
+                            {renderCarrito}
                         </tbody>
                     </table>
                 </div>
