d33088fb0d441d3cc16b2bfb40f42429ff732aac
“AndreuMartinez” <andreu.martinez.garcia.1992@gmail.com>
2022-03-19 14:49:33 +0100

    fix: Error al hacer gitadd. del nuevo componente creado

---
+++ src/Containers/Admin/dummyData.js
@@ -0,0 +1,50 @@
+export const userData = [
+    {
+      name: "Jan",
+      "Active User": 0,
+    },
+    {
+      name: "Feb",
+      "Active User": 0,
+    },
+    {
+      name: "Mar",
+      "Active User": 3,
+    },
+    {
+      name: "Apr",
+      "Active User": 0,
+    },
+    {
+      name: "May",
+      "Active User": 0,
+    },
+    {
+      name: "Jun",
+      "Active User": 0,
+    },
+    {
+      name: "Jul",
+      "Active User": 0,
+    },
+    {
+      name: "Agu",
+      "Active User": 0,
+    },
+    {
+      name: "Sep",
+      "Active User": 0,
+    },
+    {
+      name: "Oct",
+      "Active User": 0,
+    },
+    {
+      name: "Nov",
+      "Active User": 0,
+    },
+    {
+      name: "Dec",
+      "Active User": 0,
+    },
+  ];
