14c6c4e25b7826015b84eff3b73eb0772c4fc0d7
“AndreuMartinez” <andreu.martinez.garcia.1992@gmail.com>
2022-03-20 02:31:52 +0100

    feat: Finalizado Container BorrarPeliculas funcion de eliminar individualmente implementada

--- src/Containers/BorrarPelicula/BorrarPelicula.jsx
+++ src/Containers/BorrarPelicula/BorrarPelicula.jsx
@@ -13,10 +13,10 @@ const BorrarPelicula = (props) => {
     let navigate = useNavigate()
 
 
-    const [Usuario, setUsuario] = useState([])
+    const [Pelicula, setPelicula] = useState([])
 
     useEffect(() => {
-        traerUsuarios()
+        traerPelicula()
     }, [])
 
     useEffect(() => {
@@ -32,8 +32,9 @@ const BorrarPelicula = (props) => {
             headers: { Authorization: `Bearer ${props.credentials.token}` }
         };
 
+        console.log( "ESTO ES BOY")
         try {
-            let resultado = await axios.delete(`https://movie-db-geekshubs.herokuapp.com/usuarios/${value.id}`, config);
+            let resultado = await axios.delete(`https://movie-db-geekshubs.herokuapp.com/peliculas/remove/${value.id}`, config);
             alert("Usuario Eliminado con exito")
             window.location.reload()
 
@@ -44,7 +45,7 @@ const BorrarPelicula = (props) => {
     }
 
 
-    const traerUsuarios = async () => {
+    const traerPelicula = async () => {
 
         let config = {
             headers: { Authorization: `Bearer ${props.credentials.token}` }
@@ -52,23 +53,23 @@ const BorrarPelicula = (props) => {
 
         try {
 
-            let resultado = await axios.get(`https://movie-db-geekshubs.herokuapp.com/usuarios`, config);
-            setUsuario(resultado.data)
-
+            let resultado = await axios.get(`https://movie-db-geekshubs.herokuapp.com/peliculas`, config);
+            console.log(resultado.data)
+            setPelicula(resultado.data)
         } catch (error) {
             console.log(error)
         }
     }
-    const renderUsuario = Usuario.map((value, index) => {
+    const renderPelicula = Pelicula.map((value, index) => {
 
         return <tr key={index}>
 
 
 
-            <td>{value.nombre} {value.apellido}</td>
+            <td>{value.id}</td>
 
-            <td>{value.email}</td>
-            <td>{value.telefono}</td>
+            <td>{value.titulo}</td>
+            <td>{value.fecha}</td>
             <td><Button key={value.id} onClick={() => onClickDelete(value)} type="primary" danger>
                 Eliminar
             </Button></td>
@@ -87,18 +88,18 @@ const BorrarPelicula = (props) => {
             <SideBarAdmin />
             <div className="userUpdate2 widthPedidos">
                 <div className="widgetLg">
-                    <h1>Borrar Usuarios</h1>
+                    <h1>Borrar Peliculas</h1>
                     <table className="widgetLgTable">
                         <thead>
                             <tr className="widgetLgTr">
-                                <th className="widgetLgTh">Nombre</th>
-                                <th className="widgetLgTh">Email</th>
-                                <th className="widgetLgTh">Telefono</th>
+                                <th className="widgetLgTh">Id</th>
+                                <th className="widgetLgTh">Titulo</th>
+                                <th className="widgetLgTh">Fecha Estreno</th>
                                 <th className="widgetLgTh">Acción</th>
                             </tr>
                         </thead>
                         <tbody>
-                            {renderUsuario}
+                            {renderPelicula}
                         </tbody>
                     </table>
                 </div>
